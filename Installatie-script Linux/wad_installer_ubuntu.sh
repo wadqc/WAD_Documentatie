7878802fc2e43193d8078fda208603eba4f9c7d7
Tim de Wit <t.c.dewit@amc.uva.nl>
2014-02-19 17:07:23 +0100

    Nog paar haakjes vergeten...

--- Installatie-script Linux/wad_installer_ubuntu.sh
+++ Installatie-script Linux/wad_installer_ubuntu.sh
@@ -342,7 +342,7 @@ if [ "$distro" == "Ubuntu" ]; then
 	echo "service <servicename> <command>"
 	echo
 	echo "e.g. service WAD-Collector start"
-elif [ "$distro" == "Debian" || "$distro" == "LinuxMint" ]; then
+elif [[ "$distro" == "Debian" || "$distro" == "LinuxMint" ]]; then
 	echo "/etc/init.d/<servicename> <command>"
 	echo
 	echo "e.g. /etc/init.d/WAD-Collector start"
@@ -357,7 +357,7 @@ echo
 
 if [ "$distro" == "Ubuntu" ]; then
 	echo "Logfiles can be found under /var/log/upstart/<servicename>.log"
-elif [ "$distro" == "Debian" || "$distro" == "LinuxMint" ]; then
+elif [[ "$distro" == "Debian" || "$distro" == "LinuxMint" ]]; then
 	echo "Logfiles can be found under:"
 	echo "   $TARGET_WAD_SERVICES/WAD_Services/WAD_Collector/dist"
 	echo "   $TARGET_WAD_SERVICES/WAD_Services/WAD_Selector/dist"
